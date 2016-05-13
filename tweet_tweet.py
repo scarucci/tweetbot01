addc131ea4ec594ba99577ab136741f64f06b573
Simon Carucci <simoncarucci@gmail.com>
2016-05-13 15:45:11 +0100

    Add files via upload

---
+++ tweet_tweet.py
@@ -0,0 +1,26 @@
+# import twython library
+
+from twython import Twython
+
+APP_KEY = 'XXXX'
+APP_SECRET = 'XXXX'
+OAUTH_TOKEN = 'XXXX'
+OAUTH_TOKEN_SECRET = 'XXXX'
+
+twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+
+search = twitter.search(q='simoncarucci', count=5)
+
+tweets_searched = search['statuses']
+
+for tweet in tweets_searched:
+    print tweet['id_str'], '\n', tweet['text'], '\n\n\n'
+
+# input -> new post
+
+new_tweet = raw_input()
+
+twitter.update_status(status=new_tweet)
+
+
+
